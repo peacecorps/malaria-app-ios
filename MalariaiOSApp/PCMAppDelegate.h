5cba85465e38ff906f7d62f4a185c3c352ea1c31
Shruti Gupta <shruti22011991@gmail.com>
2014-06-04 22:19:52 +0530

    Initial Commit

---
+++ MalariaiOSApp/PCMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCMAppDelegate.h
+//  MalariaiOSApp
+//
+//  Created by shruti gupta on 04/06/14.
+//  Copyright (c) 2014 Shruti Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MalariaiOSApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MalariaiOSApp
+//
+//  Created by shruti gupta on 04/06/14.
+//  Copyright (c) 2014 Shruti Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCMAppDelegate class]));
+    }
+}

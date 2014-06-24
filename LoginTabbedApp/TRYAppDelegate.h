98bd5ebf375a5f853ea215b3f73a169f29dae82b
Shruti Gupta <shruti22011991@gmail.com>
2014-06-24 11:53:27 +0530

    Initial Commit

---
+++ LoginTabbedApp/TRYAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TRYAppDelegate.h
+//  LoginTabbedApp
+//
+//  Created by shruti gupta on 24/06/14.
+//  Copyright (c) 2014 Shruti Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ LoginTabbedApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoginTabbedApp
+//
+//  Created by shruti gupta on 24/06/14.
+//  Copyright (c) 2014 Shruti Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TRYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TRYAppDelegate class]));
+    }
+}

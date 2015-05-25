1d08cf15e718e7f7dfe297c274d5b344a7be8972
bphenriques <brunoaphenriques@gmail.com>
2015-05-25 22:31:42 +0100

    Added many icons; Started Setup Screen

---
+++ malaria-ios/SetupScreenViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SetupScreenViewController.swift
+//  malaria-ios
+//
+//  Created by Bruno Henriques on 25/05/15.
+//  Copyright (c) 2015 Bruno Henriques. All rights reserved.
+//
+
+import Foundation

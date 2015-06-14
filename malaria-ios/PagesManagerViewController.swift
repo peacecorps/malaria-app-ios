8150aa9d25460d70c13b2d60339c2456a14d0013
bphenriques <brunoaphenriques@gmail.com>
2015-06-14 14:49:56 +0100

    Updated storyboard

--- malaria-ios/PagesManagerViewController.swift
+++ malaria-ios/PagesManagerViewController.swift
@@ -60,7 +60,7 @@ class PagesManagerViewController : UIViewController, UIPageViewControllerDataSou
         
         //add pretended view to the hierarchy
         
-        
+        /*
         if let pretendedView = contentView{
             pretendedView.view.backgroundColor = UIColor.redColor()
             pretendedView.willMoveToParentViewController(self)
@@ -69,7 +69,7 @@ class PagesManagerViewController : UIViewController, UIPageViewControllerDataSou
             pretendedView.didMoveToParentViewController(self)
         }else{
             Logger.Error("Couldn't load pageManagerContent")
-        }
+        }*/
     }
     
     @IBAction func homeButtonHandler(){

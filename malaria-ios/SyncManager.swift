67f6007d362ed5e647ea9f5d56d855c222946037
bphenriques <brunoaphenriques@gmail.com>
2015-06-16 01:09:35 +0100

    Added (not tested) remoteFetch from API

--- malaria-ios/SyncManager.swift
+++ malaria-ios/SyncManager.swift
@@ -6,6 +6,43 @@ class SyncManager {
     
     }
     
+    private func remoteFetch(endpoint: Endpoint){
+        //TODO: Check internet connection before doing request
+        let cds = CoreDataStore.sharedInstance
+        let cdh = CoreDataHelper.sharedInstance
+        
+        var requestUrl = cds.objectManager!.requestWithObject(
+            nil,
+            method: RKRequestMethod.GET,
+            path: endpoint.path,
+            parameters: endpoint.parameters())
+        
+        var request = cds.objectManager!.managedObjectRequestOperationWithRequest(
+            requestUrl,
+            managedObjectContext: cdh.backgroundContext,
+            success:
+            {
+                (RKObjectRequestOperation, RKMappingResult) -> Void in
+                Logger.Info("Success");
+            },
+            failure: {
+                (RKObjectRequestOperation, error) -> Void in
+                Logger.Error("Error: \(error.description)")
+            })
+        
+        request.start()
+    }
+
+    private func registerMapping(endpoint: Endpoint) {
+        let responseDescriptor = RKResponseDescriptor(
+            mapping: endpoint.mapping,
+            method: RKRequestMethod.GET,
+            pathPattern: endpoint.path,
+            keyPath: nil,
+            statusCodes: NSIndexSet(index: 200))
+        CoreDataStore.sharedInstance.objectManager!.addResponseDescriptor(responseDescriptor)
+    }
+    
     func registerEndpoint(e: Endpoint){
         endpoints.updateValue(e, forKey: e.path)
     }

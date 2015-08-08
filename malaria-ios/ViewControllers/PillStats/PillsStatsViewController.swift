9b5d87d9b01751be723227bb177f363d24c6e9c6
bphenriques <brunoaphenriques@gmail.com>
2015-08-08 12:44:05 +0100

    Moved CircularProgressView to distint cocoa pods module

--- malaria-ios/ViewControllers/PillStats/PillsStatsViewController.swift
+++ malaria-ios/ViewControllers/PillStats/PillsStatsViewController.swift
@@ -1,6 +1,7 @@
 import Foundation
 import UIKit
 import Charts
+import CircleProgressView
 
 @IBDesignable class PillsStatsViewController : UIViewController, PresentsModalityDelegate {
     @IBOutlet weak var adherenceSliderTable: UITableView!
